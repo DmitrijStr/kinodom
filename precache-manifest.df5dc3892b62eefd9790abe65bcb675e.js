1d9051aa09614ddd67681e1698720246bafcd1a4
Dmitrij <s.d144@yandex.by>
2020-10-23 18:49:38 +0300

    Updates

---
+++ precache-manifest.df5dc3892b62eefd9790abe65bcb675e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11012314acae74dab6d84ec60c1c2f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7dc9a86610a05784d020",
+    "url": "./static/css/main.e7c15e52.chunk.css"
+  },
+  {
+    "revision": "0d4e7cb3da4bed9b72c7",
+    "url": "./static/js/2.1278dcf9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.1278dcf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc9a86610a05784d020",
+    "url": "./static/js/main.8ff08759.chunk.js"
+  },
+  {
+    "revision": "d7f71506d352fb69cbbb",
+    "url": "./static/js/runtime-main.615c58b1.js"
+  },
+  {
+    "revision": "a79b68fcda60ce8f337f51d3220ef2a5",
+    "url": "./static/media/Montserrat-Bold.a79b68fc.woff"
+  },
+  {
+    "revision": "d194e50992ca40d4752c6649f8684575",
+    "url": "./static/media/Montserrat-Medium.d194e509.woff"
+  },
+  {
+    "revision": "de4a911e831ca01751ab001a54de34dd",
+    "url": "./static/media/Montserrat-Regular.de4a911e.woff"
+  },
+  {
+    "revision": "3d2d13853665147fb8c2353d61c077ce",
+    "url": "./static/media/Montserrat-SemiBold.3d2d1385.woff"
+  },
+  {
+    "revision": "50146312a807433db2e5befe2e633177",
+    "url": "./static/media/enter.50146312.svg"
+  },
+  {
+    "revision": "b526604a052c238f2f7cc29646e82afa",
+    "url": "./static/media/library.b526604a.svg"
+  },
+  {
+    "revision": "bbfd7d6b0218ed441ef9a2fcca677a42",
+    "url": "./static/media/logo.bbfd7d6b.svg"
+  },
+  {
+    "revision": "1427c4f11872171e8f60b582937047f7",
+    "url": "./static/media/person.1427c4f1.svg"
+  },
+  {
+    "revision": "e44290fb2fc3c647adcdcc5514fbc4c1",
+    "url": "./static/media/saleImg.e44290fb.jpg"
+  },
+  {
+    "revision": "b15a25105167703a9f98f6f80e08b0e9",
+    "url": "./static/media/search.b15a2510.svg"
+  },
+  {
+    "revision": "9a2d7612e90f609ec2481edaf869527f",
+    "url": "./static/media/shoushenk.9a2d7612.jpg"
+  },
+  {
+    "revision": "d7a776ba0ad0cfa65ad5f2dd242f4eec",
+    "url": "./static/media/student2.d7a776ba.png"
+  },
+  {
+    "revision": "53077152fbb26cb3574f0088cdc0b9ce",
+    "url": "./static/media/vodyanoi.53077152.jpg"
+  },
+  {
+    "revision": "178e9447c0660bfae8332e0ecfaac54d",
+    "url": "./static/media/world.178e9447.svg"
+  },
+  {
+    "revision": "e8565ed10e79befcac847a0bad5d7e90",
+    "url": "./static/media/Фоновая картинка.e8565ed1.jpg"
+  }
+]);
